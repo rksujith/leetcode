420ed25ec4aa90bd913dcff53eb7d1eeecae4d41
rksujith <rksujith2005@gmail.com>
2026-02-19 12:43:17 +0530

    Create square root.c

---
+++ square root.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int mySqrt(int x) {
+    if(x==0||x==1)
+    return x;
+    long left=1,right=x,mid;
+    int ans=0;
+    while(left<=right){
+        mid=left+(right-left)/2;
+        if(mid*mid==x)
+        return mid;
+        else if(mid*mid<x){
+            ans=mid;
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+    }
+    return ans;
+    
+}
